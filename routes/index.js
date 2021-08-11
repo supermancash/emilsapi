4610053d8c1c657b82a30aebc610b87f71cadc50
etriest <emil.triest@telekom.de>
2021-08-11 19:32:41 +0200

    changes for azure

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/version/pushPackages/web.app.netlify.push', function(req, res){
+  res.header({"Content-type":"application/zip"});
+  res.sendFile();
+});
+
 module.exports = router;
